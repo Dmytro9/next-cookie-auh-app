c9ee34a6fb514cd9154a4fd29ca18e4024c38e9c
Dmytro Poliuliuk <dmytro.poliuliuk@ironsrc.com>
2019-12-03 16:48:25 +0200

    /api/login route cookie

--- server.js
+++ server.js
@@ -1,12 +1,57 @@
 const next = require("next");
 const express = require("express");
+const axios = require("axios");
+const cookieParser = require("cookie-parser");
 
 const dev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3000;
 const app = next({ dev });
+const handle = app.getRequestHandler();
+
+const AUTH_USER_TYPE = 'authenticated';
+const COOKIE_SECRET = 'ssdcdgb'; // to env file
+const COOKIE_OPTIONS = {
+  // domain:
+  httpOnly: true, // get access to cookies only on server
+  secure: !dev,
+  signed: true // can verify the source of the cookie
+}
+
+const authenticate = async (email, password) => {
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typecode.com/users"
+  );
+  return data.find(user => {
+    if (user.email === email && user.website === password) {
+      return user;
+    }
+  });
+};
 
 app.prepare().then(() => {
   const server = express();
+
+  server.use(express.json());
+  server.use(cookieParser(COOKIE_SECRET));
+
+  server.post("/api/login", async (req, res) => {
+    const { email, password } = req.body;
+    const user = await authenticate(email, password);
+    if (!user) {
+      return res.status(403).send("Invalid email or password");
+    }
+    const userData = {
+      name: user.name,
+      email: user.email,
+      type: AUTH_USER_TYPE
+    };
+    res.cookie('token', userData, COOKIE_OPTIONS);
+    res.json(userData);
+  });
+
+  server.get("*", (req, res) => {
+    return handle(req, res);
+  });
 
   server.listen(port, err => {
     if (err) throw err;
